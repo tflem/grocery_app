6e17d323b87d8a2bbebe1ea05b265e38e23f1876
Tim Fleming <tfleming26@hotmail.com>
2017-12-11 11:19:59 -0600

    Add test for validation of email attribute

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -9,4 +9,9 @@ class UserTest < ActiveSupport::TestCase
   test "should be valid" do
     assert @user.valid?
   end
+
+  test "email should be present" do
+    @user.email = ""
+    assert_not @user.valid?
+  end
 end

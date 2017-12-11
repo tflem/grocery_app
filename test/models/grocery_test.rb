e610f1d2653da32b681264b847777fe30518bada
Tim Fleming <tfleming26@hotmail.com>
2017-12-11 11:18:34 -0600

    Add test for validation of item attribute

--- test/models/grocery_test.rb
+++ test/models/grocery_test.rb
@@ -3,10 +3,15 @@ require 'test_helper'
 class GroceryTest < ActiveSupport::TestCase
 
   def setup
-    @item = Grocery.new(item: "Milk", quantity: 2)
+    @groceries = Grocery.new(item: "Milk", quantity: 2)
   end
 
   test "should be valid" do
-    assert @item.valid?
+    assert @groceries.valid?
+  end
+
+  test "item should be present" do
+    @groceries.item = "     "
+    assert_not @groceries.valid?
   end
 end

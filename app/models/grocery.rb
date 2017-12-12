d84c754afb19b52673479f89cd220739c82ae1eb
Tim Fleming <tfleming26@hotmail.com>
2017-12-12 10:01:47 -0600

    Add length and whole number validation for item and quantity attributes

--- app/models/grocery.rb
+++ app/models/grocery.rb
@@ -1,4 +1,4 @@
 class Grocery < ApplicationRecord
-  validates :item,     presence: true
-  validates :quantity, presence: true
+  validates :item,     presence: true, length: { maximum: 50 }
+  validates :quantity, presence: true, numericality: { only_integer: true }
 end

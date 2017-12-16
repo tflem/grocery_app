821d94a9cb5b77c39a32fde602948280393ffc6a
Tim Fleming <tfleming26@hotmail.com>
2017-12-16 01:13:04 -0600

    Create grocery model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,8 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
   def setup
-    @user = User.new(email: "user@grocery.com", password: "123456")
+    @user = User.new(email: "user@grocery.com", password: "123456",
+                     password_confirmation: "123456")
   end
 
   test "should be valid" do
@@ -12,6 +13,16 @@ class UserTest < ActiveSupport::TestCase
 
   test "email should be present" do
     @user.email = ""
+    assert_not @user.valid?
+  end
+
+  test "password should be present" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
   end
 end

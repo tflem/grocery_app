4856a6ebfeede0cde6863a96ebc281a2191a50a3
Tim Fleming <tfleming26@hotmail.com>
2017-12-20 09:52:40 -0600

    Add show action with controller params

--- app/controllers/groceries_controller.rb
+++ app/controllers/groceries_controller.rb
@@ -1,8 +1,22 @@
 class GroceriesController < ApplicationController
+
+  def show
+    @item = Grocery.find(params[:id])
+  end
+
   def new
     @item = Grocery.new
   end
 
   def create
+    @item = Grocery.new(grocery_params)
+    @item.save
+    redirect_to @item
   end
+
+  private
+
+    def grocery_params
+      params.require(:grocery).permit(:item, :quantity)
+    end
 end

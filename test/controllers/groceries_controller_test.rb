36a976048bf2e1ad4a3d589eae924f7ef80f047d
Tim Fleming <tfleming26@hotmail.com>
2018-02-03 21:20:31 -0600

    Add controller tests for edit and update actions

--- test/controllers/groceries_controller_test.rb
+++ test/controllers/groceries_controller_test.rb
@@ -1,7 +1,27 @@
-require "test_helper"
+require 'test_helper'
 
-describe GroceriesController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+class GroceriesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @item = groceries(:first_item)
+  end
+
+  test "should get new" do
+    sign_in users(:dash)
+    get new_grocery_path
+    assert_response :success
+  end
+
+  test "should redirect item edit when not logged in" do
+    get edit_grocery_path(@item)
+    assert_not flash.empty?
+    assert_redirected_to new_user_session_path
+  end
+
+  test "should redirect item update when not logged in" do
+    patch grocery_path(@item), params: { grocery: { item:     @item.item,
+                                                    quantity: @item.quantity } }
+    assert_not flash.empty?
+    assert_redirected_to new_user_session_path
+  end
 end

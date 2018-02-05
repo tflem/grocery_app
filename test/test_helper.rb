217f9206b06c8fef6448742ef6822cec30501956
Tim Fleming <tfleming26@hotmail.com>
2018-02-05 13:00:33 -0600

    Remove devise helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -19,7 +19,3 @@ class ActiveSupport::TestCase
   fixtures :all
   # Add more helper methods to be used by all tests here...
 end
-
-class ActionDispatch::IntegrationTest
-  include Devise::Test::IntegrationHelpers
-end

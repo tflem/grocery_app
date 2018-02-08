756c5f95dc714d1d28e8db61f847258517ee8257
Tim Fleming <tfleming26@hotmail.com>
2018-02-08 09:15:50 -0600

    Add authorization tests

--- app/controllers/groceries_controller.rb
+++ app/controllers/groceries_controller.rb
@@ -1,23 +1,28 @@
 class GroceriesController < ApplicationController
-  
+
   def index
     @all_items = Grocery.all
+    authorize @all_items
   end
 
   def show
     @item = Grocery.find(params[:id])
+    authorize @item
   end
 
   def new
     @item = Grocery.new
+    authorize @item
   end
 
   def edit
     @item = Grocery.find(params[:id])
+    authorize @item
   end
 
   def create
     @item = Grocery.new(grocery_params)
+    authorize @item
     if @item.save
       flash[:success] = "Grocery Item Added!"
       redirect_to @item

3094d7366b275a22b5534f169ff13b760c51fbc3
Tim Fleming <tfleming26@hotmail.com>
2018-02-10 05:11:47 -0600

    Add test for successfull deletion

--- test/integration/groceries_index_test.rb
+++ test/integration/groceries_index_test.rb
@@ -5,9 +5,10 @@ class GroceriesIndexTest < ActionDispatch::IntegrationTest
 
   def setup
     @user = users(:dash)
+    @item = groceries(:first_item)
   end
 
-  test "index including pagination" do
+  test "index including pagination with delete link" do
     sign_in @user
     get groceries_path
     assert_template "groceries/index"
@@ -15,5 +16,10 @@ class GroceriesIndexTest < ActionDispatch::IntegrationTest
     Grocery.paginate(page: 1).each do |grocery|
       assert_select "td.grocery-items", text: grocery.item
     end
+    assert_difference "Grocery.count", -1 do
+      delete grocery_path(@item)
+    end
+    assert_not flash.empty?
+    assert_redirected_to groceries_path
   end
 end
